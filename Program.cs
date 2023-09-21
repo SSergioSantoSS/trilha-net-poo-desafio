9e53087adc37a07ad9f437eebc504f868a2b14a9
SSergioSantoSS <sergiomi87st@gmail.com>
2023-09-21 20:26:09 -0300

    concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criar uma instância de Nokia
+            var nokia = new Nokia(numero:"12345", modelo: "Nokia 3310", imei: "123456789", memoria: 16);
+
+            // Criar uma instância de Iphone
+            var iphone = new Iphone(numero:"67890", modelo:"iPhone 12", imei:"987654321", memoria: 128);                      
+
+            // Realizar ações com os smartphones
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("*****************************");
+            Console.WriteLine("\n");
+
+            iphone.Ligar(); 
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+
+            Console.ReadKey();
+        }
+    }
+}
